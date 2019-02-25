2f1e7e74738d045d8d304a91dcad3346581f120b
Noah Ross <noah.baine@gmail.com>
2019-02-25 15:45:33 -0800

    basic components for new deck and selection

---
+++ components/AddNewDeck.js
@@ -0,0 +1,12 @@
+import  React, { Component } from 'react';
+import {StyleSheet, View, Text} from 'react-native'
+
+class AddNewDeck extends Component {
+    render() {
+        return (
+            <Text>Add New Deck</Text>
+        )
+    }
+}
+
+export default AddNewDeck
---
+++ components/SelectDeck.js
@@ -0,0 +1,12 @@
+import  React, { Component } from 'react';
+import {StyleSheet, View, Text} from 'react-native'
+
+class SelectDeck extends Component {
+    render() {
+        return (
+            <Text>Select Deck</Text>
+        )
+    }
+}
+
+export default SelectDeck
