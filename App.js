550e5d24a48f1849b6bd51309979d4eac9557015
Noah Ross <noah.baine@gmail.com>
2019-02-25 16:20:26 -0800

    added basic navigation

--- App.js
+++ App.js
@@ -8,6 +8,32 @@ import deckStuff from "./reducers"
 
 
 import SelectDeck from "./components/SelectDeck"
+import AddNewDeck from "./components/AddNewDeck"
+
+const Nav = TabNavigator({
+  SelectDeck: {
+    screen: SelectDeck,
+    navigationOptions: {
+      tabBarLabel: "Select Deck"
+    }
+  },
+  AddNewDeck: {
+    screen: AddNewDeck,
+    navigationOptions: {
+      tabBarLabel: "Add Deck"
+    }
+  }
+},{
+  navigationOptions: {
+    header:null
+  }
+})
+
+const MainStack = StackNavigator({
+  Home: {
+    screen: Nav
+  }
+})
 
 
 export default class App extends React.Component {
@@ -15,6 +41,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(deckStuff)}>
+      <MainStack />
       </Provider>
     )
   }
